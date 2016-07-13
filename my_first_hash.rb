b9c1ab259c7b5906650af71fb6030ea4d43eb6a3
hafizrahman1 <hafiz.bd2009@gmail.com>
2016-07-13 10:09:47 -0400

    first hash

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  my_hash = {"Name" => "Hafziur"}
+  my_hash = {"Name" => "Hafizur"}
   
 end
 
